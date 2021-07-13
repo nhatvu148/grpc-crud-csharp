e97c38b0feafd1dd29c3e884c12267931180d280
nhatvu148 <nhatvu148@gmail.com>
2021-07-14 00:52:34 +0900

    initial commit

---
+++ server/Program.cs
@@ -0,0 +1,43 @@
+﻿using Blog;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Services = {
+                        BlogService.BindService(new BlogServiceImpl()),
+                    },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listening on port: " + Port);
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start:" + e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
